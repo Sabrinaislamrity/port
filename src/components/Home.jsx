92c1efa017215041df7d3b97705163607c2f2ae3
sabrina islam <sabrinaislamrity@gmail.com>
2025-06-30 19:09:50 +0600

    three

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Banner from './Banner';
 import About from '../pages/About';
+import Skill from '../pages/Skill';
+import Projects from '../pages/Projects';
+import Contact from '../pages/Contact';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <About></About>
+              <Skill></Skill>
+            <Projects></Projects>
+            <Contact></Contact>
             
         </div>
     );
